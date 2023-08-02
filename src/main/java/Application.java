67823b29cdbd7f2dab19e09224c5c8141669e1a7
Alex Daughters <alexdaughters@gmail.com>
2023-08-02 13:53:34 +0000

    completed hello world

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,7 @@
 
 public class Application {
     /**
+     * 
      * This class contains a main method that allows you to manually test the Hello World lab functionality.
      * No changes you make here affect the test case results. You must still run & pass the test cases.
      * All implementation code should be written in the Lab class.
